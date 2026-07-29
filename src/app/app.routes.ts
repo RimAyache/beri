94aab9a31cc67652bf68e339147a7a8b0b377c3f
RimAyache <ayashrim4@gmail.com>
2026-07-29 21:05:54 +0300

    add lazy loadingg

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,27 +2,42 @@ import { Routes } from '@angular/router';
 
 import { StorefrontLayoutComponent } from './core/layout/storefront-layout.component';
 import { AdminLayoutComponent } from './core/layout/admin-layout.component';
-import { ProductsComponent } from './pages/products/products.component';
-import { LoginComponent } from './pages/login/login.component';
-import { SignupComponent } from './pages/signup/signup.component';
-import { NotFoundComponent } from './pages/not-found/not-found.component';
-import { AdminComponent } from './pages/admin/admin.component';
 
 export const routes: Routes = [
   {
     path: '',
     component: StorefrontLayoutComponent,
     children: [
-      { path: '', component: ProductsComponent },
-      { path: 'login', component: LoginComponent },
-      { path: 'register', component: SignupComponent },
-      { path: 'not-found', component: NotFoundComponent },
+      {
+        path: '',
+        loadComponent: () =>
+          import('./pages/products/products.component').then((m) => m.ProductsComponent),
+      },
+      {
+        path: 'login',
+        loadComponent: () => import('./pages/login/login.component').then((m) => m.LoginComponent),
+      },
+      {
+        path: 'register',
+        loadComponent: () =>
+          import('./pages/signup/signup.component').then((m) => m.SignupComponent),
+      },
+      {
+        path: 'not-found',
+        loadComponent: () =>
+          import('./pages/not-found/not-found.component').then((m) => m.NotFoundComponent),
+      },
     ],
   },
   {
     path: 'admin',
     component: AdminLayoutComponent,
-    children: [{ path: '', component: AdminComponent }],
+    children: [
+      {
+        path: '',
+        loadComponent: () => import('./pages/admin/admin.component').then((m) => m.AdminComponent),
+      },
+    ],
   },
   { path: '**', redirectTo: 'not-found' },
 ];
